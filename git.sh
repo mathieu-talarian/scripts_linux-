187c3e0b87e3a9aebb18a6019da80c706602da82
Mathieu Moullec <mathmoul@hotmail.fr>
2019-05-06 10:03:57 +0200

    I had a cup of tea and now it's fixed

--- git.sh
+++ git.sh
@@ -4,4 +4,5 @@ git config --global user.name "Mathieu Moullec"
 git config --global user.email "mathmoul@hotmail.fr"
 sudo git config --system core.editor nvim
 git config --global credential.helper cache
+git config --global credential.helper store
 git config --global credential.helper 'cache --timeout=3600'
